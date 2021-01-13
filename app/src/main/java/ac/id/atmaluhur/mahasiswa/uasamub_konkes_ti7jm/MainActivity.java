5c98febbe7c8f20c51f53f07a231d4d3c2f58a91
emasumaya <emas1541@gmail.com>
2021-01-13 22:25:08 +0700

    upload project uas

---
+++ app/src/main/java/ac/id/atmaluhur/mahasiswa/uasamub_konkes_ti7jm/MainActivity.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.mahasiswa.uasamub_konkes_ti7jm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

a8cd7dd3de29cdabc720f3fee73909fdf5482afa
SanandreBS <san.andre2302@gmail.com>
2021-01-14 18:25:26 +0700

    sanandre

--- app/src/main/java/ac/id/atmaluhur/mahasiswa/uasamub_konkes_ti7jm/BerhasilAct.java
+++ app/src/main/java/ac/id/atmaluhur/mahasiswa/uasamub_konkes_ti7jm/BerhasilAct.java
@@ -1,14 +1,30 @@
-package ac.id.atmaluhur.mahasiswa.uasamub_konkes_ti7jm;
+package ac.id.atmaluhur.uasamub_konkes_ti7jm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BerhasilAct extends AppCompatActivity {
+
+    Button btn_lanjut;
+    ImageView icon_daftar_berhasil;
+    TextView app_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_berhasil);
+        icon_daftar_berhasil = findViewById(R.id.icon_daftar_berhasil);
+        app_title = findViewById(R.id.app_title);
+        btn_lanjut = findViewById(R.id.btn_lanjut);
+    }
+    public void berhasildaftarkursus(View view) {
+        Intent gotohome = new Intent(this, LoginAct.class);
+        startActivity(gotohome);
     }
 }
--- app/src/main/java/ac/id/atmaluhur/mahasiswa/uasamub_konkes_ti7jm/MainActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mahasiswa/uasamub_konkes_ti7jm/MainActivity.java
@@ -1,14 +1,66 @@
-package ac.id.atmaluhur.mahasiswa.uasamub_konkes_ti7jm;
+package ac.id.atmaluhur.uasamub_konkes_ti7jm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btn1, btn2,
+            btn3, btn_keluar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn_keluar = findViewById(R.id.btn_keluar);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotocov = new Intent(MainActivity.this, CovAct.class);
+                startActivity(gotocov);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotodaftar = new Intent(MainActivity.this, DaftarAct.class);
+                startActivity(gotodaftar);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoinfo = new Intent(MainActivity.this, InfoAct.class);
+                startActivity(gotoinfo);
+            }
+        });
+
+        btn_keluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotologin = new Intent(MainActivity.this, LoginAct.class);
+                startActivity(gotologin);
+            }
+        });
     }
 }
+
